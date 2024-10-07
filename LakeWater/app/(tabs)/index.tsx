359c9f381c1e44b5c7a554ed8d4bbe3e8651c342
Ravi <123022068+ravi-ray96@users.noreply.github.com>
2024-10-07 15:58:53 +0000

    Update

--- LakeWater/app/(tabs)/index.tsx
+++ LakeWater/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, Platform, StyleSheet } from 'react-native';
+import { Image, StyleSheet, Platform } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Hello Current Keepers!</ThemedText>
+        <ThemedText type="title">Hello!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
